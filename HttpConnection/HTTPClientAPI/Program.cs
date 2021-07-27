f12748e1d8aca1c7904242ac853ba2e24a3e2772
pratyushashukla <pratyusha.shukla212@gmail.com>
2021-07-27 18:18:13 +0530

    Changes made

---
+++ HttpConnection/HTTPClientAPI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using HttpWebApi;
+
+namespace HTTPClientAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:/44334/weatherforecast/");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage resmsg = client.GetAsync("api/Values").Result;
+            if(resmsg.IsSuccessStatusCode)
+            {
+                var products = resmsg.Content.ReadAsStringAsync().Result;
+            }
+            else
+            {
+                Console.WriteLine("{0} {1}",(int) resmsg.StatusCode,resmsg);
+            }
+        }
+    }
+}
---
+++ HttpConnection/HttpWebApi/Controllers/ServerAPIController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServerAPIController : Controller
+    {
+        [HttpGet]
+        public List<string> Get()
+        {
+            List<string> li = new List<string>();
+            li.Add("xyz");
+            li.Add("abc");
+            li.Add("sumukh");
+            return li;
+        }
+    }
+}
